105b3ec295b8bd238e82ea12559251399a32f78e
Suraj-Razor <14635@coderacademy.edu.au>
2023-12-17 15:51:50 +1100

    Created starting file which takes input from user and creates a json file to store user input

---
+++ new_user.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+class NewUser():
+  def __init__(self, id, first_name, last_name, dob, occupation, full_home_address, email, access_pin):
+    self.user_id = id
+    self.first_name = first_name
+    self.last_name = last_name
+    self.dob = dob
+    self.occupation = occupation
+    self.full_home_address = full_home_address
+    self.email = email
+    self.access_pin = access_pin
+    new_user_data = {
+      "user_id": self.user_id,
+      "first_name": self.first_name,
+      "last_name": self.last_name,
+      "dob": self.dob,
+      "occupation": self.occupation,
+      "home_address": self.full_home_address,
+      "email": self.email,
+      "access_pin": self.access_pin
+    }
+
+    try:
+      with open("./data/user_data.json", "r") as json_file:
+        data = json.load(json_file)
+        data["user_input"].append(new_user_data)
+      with open("./data/user_data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        
+    except:
+      os.mkdir("./data")
+      with open("./data/user_data.json", "w") as json_file:
+        data = {"user_input":[new_user_data]}
+        json.dump(data, json_file, indent=4)
